f67c2efe314821ce907afa990dc402e4fa40b665
AJ <antonio.jr@outlook.com>
2017-11-03 00:51:22 -0300

    Add Tests

---
+++ CorridaVirtual.Domain.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CorridaVirtual.Domain.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CorridaVirtual.Domain.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("15df1f0b-91fd-4441-bc3f-2ca753261b99")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CorridaVirtual.Domain.Test/RegistroTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CorridaVirtual.Domain.Test
+{
+    [TestClass]
+    public class RegistroTests
+    {
+        [TestMethod]
+        public void Posso_Criar_Um_Registro()
+        {
+            var registro = new Registro();
+            Assert.IsNotNull(registro);
+        }
+    }
+}
